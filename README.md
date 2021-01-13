7928ccd8808d74b6d4b749cc5998ac0630e433b0
nguyenhoanganhvu <nguyenhoanganhvu.it@gmail.com>
2021-01-13 13:07:57 +0700

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+Hello,
+Phan mem quan ly shop giay dep viet bang C#(Winform)
---
+++ ShopGiayDep/GUI/FormHome.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace ShopGiayDep.GUI
+{
+    partial class FormHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlNoiDung = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnQuanLyBanHang = new System.Windows.Forms.Button();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnQuanLyKho = new System.Windows.Forms.Button();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.btnQuanTriCuaHang = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnImage = new System.Windows.Forms.Button();
+            this.pnlNoiDung.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlNoiDung
+            // 
+            this.pnlNoiDung.Controls.Add(this.label2);
+            this.pnlNoiDung.Controls.Add(this.btnImage);
+            this.pnlNoiDung.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlNoiDung.Location = new System.Drawing.Point(200, 0);
+            this.pnlNoiDung.Name = "pnlNoiDung";
+            this.pnlNoiDung.Size = new System.Drawing.Size(784, 561);
+            this.pnlNoiDung.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Magneto", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(204, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(408, 88);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cửa Hàng Giày Dép \r\n          Nam Nữ";
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel11.Controls.Add(this.btnAdmin);
+            this.panel11.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel11.Location = new System.Drawing.Point(0, 304);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(200, 34);
+            this.panel11.TabIndex = 15;
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnAdmin.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnAdmin.FlatAppearance.BorderSize = 0;
+            this.btnAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdmin.ForeColor = System.Drawing.Color.White;
+            this.btnAdmin.Location = new System.Drawing.Point(12, 0);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnAdmin.Size = new System.Drawing.Size(188, 34);
+            this.btnAdmin.TabIndex = 3;
+            this.btnAdmin.Text = "Quản Trị Hệ Thống";
+            this.btnAdmin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdmin.UseVisualStyleBackColor = false;
+            // 
+            // btnQuanLyBanHang
+            // 
+            this.btnQuanLyBanHang.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnQuanLyBanHang.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnQuanLyBanHang.FlatAppearance.BorderSize = 0;
+            this.btnQuanLyBanHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuanLyBanHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuanLyBanHang.ForeColor = System.Drawing.Color.White;
+            this.btnQuanLyBanHang.Location = new System.Drawing.Point(12, 0);
+            this.btnQuanLyBanHang.Name = "btnQuanLyBanHang";
+            this.btnQuanLyBanHang.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnQuanLyBanHang.Size = new System.Drawing.Size(188, 34);
+            this.btnQuanLyBanHang.TabIndex = 0;
+            this.btnQuanLyBanHang.Text = "Quản Lý Bán Hàng";
+            this.btnQuanLyBanHang.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnQuanLyBanHang.UseVisualStyleBackColor = false;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.Color.PapayaWhip;
+            this.panel12.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel12.Location = new System.Drawing.Point(0, 270);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(200, 34);
+            this.panel12.TabIndex = 14;
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.button1);
+            this.panel10.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel10.Location = new System.Drawing.Point(0, 493);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(200, 34);
+            this.panel10.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 0);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(188, 34);
+            this.button1.TabIndex = 3;
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnQuanLyKho
+            // 
+            this.btnQuanLyKho.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnQuanLyKho.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnQuanLyKho.FlatAppearance.BorderSize = 0;
+            this.btnQuanLyKho.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuanLyKho.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuanLyKho.ForeColor = System.Drawing.Color.White;
+            this.btnQuanLyKho.Location = new System.Drawing.Point(12, 0);
+            this.btnQuanLyKho.Name = "btnQuanLyKho";
+            this.btnQuanLyKho.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnQuanLyKho.Size = new System.Drawing.Size(188, 34);
+            this.btnQuanLyKho.TabIndex = 1;
+            this.btnQuanLyKho.Text = "Quản Lý Kho";
+            this.btnQuanLyKho.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnQuanLyKho.UseVisualStyleBackColor = false;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.btnLogOut);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel8.Location = new System.Drawing.Point(0, 527);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(200, 34);
+            this.panel8.TabIndex = 12;
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnLogOut.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnLogOut.FlatAppearance.BorderSize = 0;
+            this.btnLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogOut.ForeColor = System.Drawing.Color.White;
+            this.btnLogOut.Location = new System.Drawing.Point(12, 0);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnLogOut.Size = new System.Drawing.Size(188, 34);
+            this.btnLogOut.TabIndex = 4;
+            this.btnLogOut.Text = "Đăng Xuất";
+            this.btnLogOut.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogOut.UseVisualStyleBackColor = false;
+            // 
+            // btnQuanTriCuaHang
+            // 
+            this.btnQuanTriCuaHang.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnQuanTriCuaHang.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnQuanTriCuaHang.FlatAppearance.BorderSize = 0;
+            this.btnQuanTriCuaHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuanTriCuaHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuanTriCuaHang.ForeColor = System.Drawing.Color.White;
+            this.btnQuanTriCuaHang.Location = new System.Drawing.Point(12, 0);
+            this.btnQuanTriCuaHang.Name = "btnQuanTriCuaHang";
+            this.btnQuanTriCuaHang.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnQuanTriCuaHang.Size = new System.Drawing.Size(188, 34);
+            this.btnQuanTriCuaHang.TabIndex = 2;
+            this.btnQuanTriCuaHang.Text = "Quản Trị Cửa Hàng";
+            this.btnQuanTriCuaHang.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnQuanTriCuaHang.UseVisualStyleBackColor = false;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel7.Controls.Add(this.btnQuanTriCuaHang);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 236);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(200, 34);
+            this.panel7.TabIndex = 11;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.PapayaWhip;
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Location = new System.Drawing.Point(0, 202);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(200, 34);
+            this.panel6.TabIndex = 8;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel5.Controls.Add(this.btnQuanLyKho);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 168);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 34);
+            this.panel5.TabIndex = 6;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.PapayaWhip;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 134);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 34);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel3.Controls.Add(this.btnQuanLyBanHang);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 100);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 34);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PapayaWhip;
+            this.panel1.Controls.Add(this.panel11);
+            this.panel1.Controls.Add(this.panel12);
+            this.panel1.Controls.Add(this.panel10);
+            this.panel1.Controls.Add(this.panel8);
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.pnlMenu);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 561);
+            this.panel1.TabIndex = 4;
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.PapayaWhip;
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(200, 100);
+            this.pnlMenu.TabIndex = 0;
+            // 
+            // btnImage
+            // 
+            this.btnImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnImage.Image = global::ShopGiayDep.Properties.Resources.cuahanggiay1;
+            this.btnImage.Location = new System.Drawing.Point(0, 0);
+            this.btnImage.Margin = new System.Windows.Forms.Padding(2);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(784, 561);
+            this.btnImage.TabIndex = 2;
+            this.btnImage.UseVisualStyleBackColor = true;
+            // 
+            // FormHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.pnlNoiDung);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormHome";
+            this.Text = "FormHome";
+            this.pnlNoiDung.ResumeLayout(false);
+            this.pnlNoiDung.PerformLayout();
+            this.panel11.ResumeLayout(false);
+            this.panel10.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlNoiDung;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnImage;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnQuanLyBanHang;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnQuanLyKho;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Button btnQuanTriCuaHang;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlMenu;
+    }
+}
